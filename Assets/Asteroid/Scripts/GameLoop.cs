12d9add89229f62127f837af8514a46cb28161c6
Bartek <pilat.bartosz@gmail.com>
2018-07-24 23:42:37 +0200

    Add base game loop that spawns asteroids

---
+++ Assets/Asteroid/Scripts/GameLoop.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameLoop : MonoBehaviour
+{
+    public AsteroidSpawner asteroidSpawner;
+
+    
+    /// <summary>
+    /// Time to wait at beginning of the game
+    /// </summary>
+    float beginWaitTime = 1f;
+
+
+    void Start()
+    {
+        Invoke("StartGame", beginWaitTime);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+
+        asteroidSpawner.SpawnNewBig(3);
+    }
+}
