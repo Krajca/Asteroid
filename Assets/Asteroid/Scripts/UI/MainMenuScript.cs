bd5908adf76e7530df9e1e5c5ea4ca20b9276090
Bartek <pilat.bartosz@gmail.com>
2018-07-24 20:20:18 +0200

    Add basic scene division and main menu UI

---
+++ Assets/Asteroid/Scripts/UI/MainMenuScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class MainMenuScript : MonoBehaviour
+{
+    public Text hiScore;
+
+    void Start()
+    {
+        //TODO change that to file saving with settings
+        hiScore.text = PlayerPrefs.GetInt("hiScore", 0).ToString();
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("GameScene");
+    }
+
+    public void Settings()
+    {
+        SceneManager.LoadScene("Settings");
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
