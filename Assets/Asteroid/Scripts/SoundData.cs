18781b509ce2430b449366868b67203b18ded917
Bartek <pilat.bartosz@gmail.com>
2018-07-26 22:53:16 +0200

    Add SoundData

---
+++ Assets/Asteroid/Scripts/SoundData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsteroidGame.Sound
+{
+    [CreateAssetMenu(fileName = "SoundData", menuName = "AsteroidData/SoundData")]
+    public class SoundData : ScriptableObject
+    {
+        public AudioClip Shot;
+        public AudioClip Explosion;
+        public AudioClip PlayerDeath;
+        public AudioClip PlayerRespawn;
+
+        public bool RandomizeShotSound;
+        [Tooltip("Used for randomize shoot sound")]
+        public float LowPitchRange;
+        [Tooltip("Used for randomize shoot sound")]
+        public float HighPitchRange;
+    }
+}
