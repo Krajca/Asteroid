f30ea48fe57864ae57d98ce9539c10bcfeb612e4
Bartek <pilat.bartosz@gmail.com>
2018-07-27 00:58:24 +0200

    Add logo scene without logo

---
+++ Assets/Asteroid/Scripts/LogoSceneScript.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace AsteroidGame
+{
+    public class LogoSceneScript : MonoBehaviour
+    {
+
+        IEnumerator Start()
+        {
+            yield return new WaitForSeconds(1f);
+            SceneManager.LoadScene("MenuScene");
+        }
+
+    }
+}
