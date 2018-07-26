72a484ba9553453d386f5a79917291cbfe9489bb
Bartek <pilat.bartosz@gmail.com>
2018-07-26 20:41:44 +0200

    Add abstract class for inputs

---
+++ Assets/Asteroid/Scripts/Input/Input.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsteroidGame.Input
+{
+    public abstract class Input : MonoBehaviour
+    {
+        protected bool canPassInput;
+
+        public void SwitchInputTo(bool i)
+        {
+            canPassInput = i;
+        }
+    }
+
+    
+}
