4923ab9efab3b5495cab09416f87ead2541b665f
Bartek <pilat.bartosz@gmail.com>
2018-07-26 21:10:25 +0200

    Fix screen edge crossing that it will the same as video reference

--- Assets/Asteroid/Scripts/CrossEdges.cs
+++ Assets/Asteroid/Scripts/CrossEdges.cs
@@ -2,12 +2,31 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-[RequireComponent(typeof(Renderer))]
-public class CrossEdges : MonoBehaviour
+namespace AsteroidGame
 {
-    void OnBecameInvisible()
+    public class CrossEdges : MonoBehaviour
     {
-        //TODO fix: this behaviour is different from original game ???
-        transform.position = transform.position * -1;
+        Rigidbody2D rb;
+
+        float horisontalBound;
+        float verticalBound;
+
+        private void Start()
+        {
+            horisontalBound = Camera.main.aspect * Camera.main.orthographicSize;
+            verticalBound = Camera.main.orthographicSize;
+        }
+
+        private void Update()
+        {
+            if(transform.position.y > verticalBound || transform.position.y < -verticalBound)
+            {
+                transform.position = transform.position * new Vector2(1f,-0.9f);
+            }
+            if (transform.position.x > horisontalBound || transform.position.x < -horisontalBound)
+            {
+                transform.position = transform.position * new Vector2(-0.9f, 1f);
+            }
+        }
     }
 }
