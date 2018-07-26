90c16b31ab45a259b39d2e27765ac7129afc8355
Bartek <pilat.bartosz@gmail.com>
2018-07-26 22:53:02 +0200

    Add SoundPlayer

---
+++ Assets/Asteroid/Scripts/SoundPlayer.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AsteroidGame.Sound
+{
+    public class SoundPlayer : MonoBehaviour
+    {
+        public static SoundPlayer instance = null;
+
+        [SerializeField]
+        SoundData soundData;
+
+        AudioSource ShotsAudioSource;
+        AudioSource OtherAudioSource;
+
+        void Awake()
+        {
+            if (instance == null)
+                instance = this;
+            else if (instance != this)
+                Destroy(gameObject);
+
+            ShotsAudioSource = gameObject.AddComponent<AudioSource>();
+            OtherAudioSource = gameObject.AddComponent<AudioSource>();
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void PlayShotSound()
+        {
+            ShotsAudioSource.clip = soundData.Shot;
+
+            if(soundData.RandomizeShotSound)
+                ShotsAudioSource.pitch = Random.Range(soundData.LowPitchRange, soundData.HighPitchRange);
+
+            ShotsAudioSource.Play();
+        }
+        public void PlayPlayerDeathSound()
+        {
+            OtherAudioSource.clip = soundData.PlayerDeath;
+            OtherAudioSource.Play();
+        }
+        public void PlayPlayerRespawnSound()
+        {
+            OtherAudioSource.clip = soundData.PlayerRespawn;
+            OtherAudioSource.Play();
+        }
+
+        public void PlayExplosionSound()
+        {
+            OtherAudioSource.clip = soundData.Explosion;
+            OtherAudioSource.Play();
+        }
+    }
+}
